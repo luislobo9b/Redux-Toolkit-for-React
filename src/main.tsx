8da672819a005bb865609bf4884949a1c3dbf84e
Luis Lobo <luis.lobo9b@gmail.com>
2024-11-10 13:42:27 -0300

    atualizado main.tsx com com store e Provider (envolvendo todo o projeto com o redux para ter escopo global)

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// RTK
+import { store } from './store/store.ts'
+import { Provider } from 'react-redux'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
